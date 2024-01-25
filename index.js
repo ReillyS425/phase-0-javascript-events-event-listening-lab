f2b2de7d6e2537613e8f1a05787ab39132055fe6
Reilly Shofner <reilly.shofner@gmail.com>
2024-01-25 14:30:29 -0800

    complete js events event listening lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function addingEventListener() {
+//const input = document.getElementById('button');
+
+//function addingEventListener() { 
+   // alert('I was clicked!');
+   // input.addEventListener('click', addingEventListener);
+//}
+
+//input.addEventListener('click', addingEventListener);
+function addingEventListener() { const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+addingEventListener();
+
